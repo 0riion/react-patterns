19bc2c1e8d42c6fcb06ec17b7847014757e94b95
Julio Flores <juliocesarflores12@gmail.com>
2023-05-20 13:52:49 -0400

    feat: container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // import HigherOrderComponent from './patterns/HigherOrderComponent';
-import RenderProps from './patterns/RenderProps';
+// import RenderProps from './patterns/RenderProps';
+import ContainerComponent from './patterns/ContainerComponent';
 
 function App() {
-  return (<RenderProps />);
+  return (<ContainerComponent />);
 }
 
 export default App;
